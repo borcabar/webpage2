1bfc8c2dce744ae5c47c31ef157e5795f24b44b9
boris <borcabrera@hotmail.com>
2018-06-27 11:20:41 +0200

    improving urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,10 @@
+
+
 from django.conf.urls import url
-from blog import views
+from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 
-'''
-from django.conf.urls import url
-from howdy import views
-
-urlpatterns = [
-    url(r'^$', views.HomePageView.as_view()),
-    url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
-]
-'''
